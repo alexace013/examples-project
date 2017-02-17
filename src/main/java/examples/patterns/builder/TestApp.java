09f6b5273355111dbf3b49985c4f673d2e197320
Alexander Bakhin <Alexander Bakhin>
2017-02-17 21:42:39 +0200

    created TestApp class

--- src/main/java/examples/patterns/builder/TestApp.java
+++ src/main/java/examples/patterns/builder/TestApp.java
@@ -5,7 +5,7 @@ public class TestApp {
     public static void main(String[] args) {
 
         PersonCreator personCreator = new PersonCreator();
-        Person person1 = personCreator.createPerson("Name1", -1, 'm');
+        Person person1 = personCreator.createPerson("Name1", 0, 'm');
         Person person2 = personCreator.createPerson("Name2", 1, 'w');
         System.out.println(String.format("%s= %s %s", person1.getName(), person1.getAge(), person1.getSex()));
         System.out.println(String.format("%s= %s %s", person2.getName(), person2.getAge(), person2.getSex()));
