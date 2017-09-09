544be03404483081b9c23c5a38403813983650b9
alexace013 <alexace013@gmail.com>
2017-09-09 15:28:47 +0300

    added new method exampleStartsWith into StringExamples and added tests for this method into StringExamplesTest class

--- src/main/java/examples/stringsExamples/StringExamples.java
+++ src/main/java/examples/stringsExamples/StringExamples.java
@@ -3,11 +3,12 @@ package examples.stringsExamples;
 import org.apache.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
 
-public class StringExamples {
+class StringExamples {
 
     private static final Logger LOG = Logger.getLogger(StringExamples.class);
 
     private static final String NOT_ENDS_WITH = "<%s> IS NOT ENDS WITH <%s>";
+    private static final String NOT_STARTS_WITH = "<%s> IS NOT STARTS WITH <%s>";
     private static final String IS_NULL = "TEXT <%s> or ENDS <%s> is NULL!";
 
     @Nullable
@@ -16,20 +17,35 @@ public class StringExamples {
             final String ends) throws Exception {
         LOG.info(String.format("TEXT: %s", text));
         LOG.info(String.format("ENDS: %s", ends));
-        if (text == null || ends == null) {
-            try {
-                throw new NullPointerException();
-            } catch (NullPointerException e) {
-                LOG.error(String.format(IS_NULL, text, ends));
-            }
-        }
-        assert text != null;
-        assert ends != null;
+        isParametersNull(text, ends);
         if (text.endsWith(ends)) {
             return text.subSequence((text.length() - ends.length()),
                     text.length()).toString();
         }
         throw new Exception(String.format(NOT_ENDS_WITH, text, ends));
+    }
+
+    @Nullable
+    static String exampleStartsWith(final String text, final String starts) throws Exception {
+        LOG.info(String.format("TEXT: %s", text));
+        LOG.info(String.format("STARTS: %s", starts));
+        isParametersNull(text, starts);
+        if (text.startsWith(starts)) {
+            return text.subSequence(0, starts.length()).toString();
+        }
+        throw new Exception(String.format(NOT_STARTS_WITH, text, starts));
+    }
+
+    private static void isParametersNull(final String param1, final String param2) {
+        if (param1 == null || param2 == null) {
+            try {
+                throw new NullPointerException();
+            } catch (NullPointerException e) {
+                LOG.error(String.format(IS_NULL, param1, param2));
+            }
+        }
+        assert param1 != null;
+        assert param2 != null;
     }
 
 }
--- src/test/java/examples/stringsExamples/StringExamplesTest.java
+++ src/test/java/examples/stringsExamples/StringExamplesTest.java
@@ -5,10 +5,8 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
-import java.util.Arrays;
-
 import static org.junit.Assert.*;
-import static examples.stringsExamples.StringExamples.exampleEndsWith;
+import static examples.stringsExamples.StringExamples.*;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StringExamplesTest {
@@ -18,6 +16,8 @@ public class StringExamplesTest {
     private static final String TEXT = "This is the test text";
     private static final String ENDS = "ext";
     private static final String ENDS_2 = "exc";
+    private static final String STARTS = "Thi";
+    private static final String STARTS_2 = "The";
 
     @Test
     public void positiveTestStringExamplesEndsWith() throws Exception {
@@ -87,6 +87,86 @@ public class StringExamplesTest {
     public void negativeTest05StringExamplesEndsWith() throws Exception {
         try {
             assertEquals(exampleEndsWith(TEXT, ENDS), null);
+        } catch (AssertionError e) {
+            for (int i = 0; i < e.getStackTrace().length; i++) {
+                LOG.error(e.getStackTrace()[i]);
+            }
+        }
+        String methodName = Thread.currentThread()
+                .getStackTrace()[1]
+                .getMethodName()
+                .concat("()");
+        LOG.info(String.format("method <%s> passed successful", methodName));
+    }
+
+    @Test
+    public void positiveTestStringExamplesStartsWith() throws Exception {
+        assertEquals(exampleStartsWith(TEXT, STARTS), STARTS);
+        String methodName = Thread.currentThread()
+                .getStackTrace()[1]
+                .getMethodName()
+                .concat("()");
+        LOG.info(String.format("method <%s> passed successful", methodName));
+    }
+
+    @Test
+    public void negativeTest01StringExamplesStartsWith() {
+        try {
+            assertNotEquals(exampleStartsWith(TEXT, STARTS_2), STARTS);
+            String methodName = Thread.currentThread()
+                    .getStackTrace()[1]
+                    .getMethodName()
+                    .concat("()");
+            LOG.info(String.format("method <%s> passed successful", methodName));
+        } catch (Exception e) {
+            LOG.error(e);
+        }
+    }
+
+    @Test
+    public void negativeTest02StringExamplesStartsWith() throws Exception {
+        assertNotEquals(exampleStartsWith(TEXT, STARTS), STARTS_2);
+        String methodName = Thread.currentThread()
+                .getStackTrace()[1]
+                .getMethodName()
+                .concat("()");
+        LOG.info(String.format("method <%s> passed successful", methodName));
+    }
+
+    @Test
+    public void negativeTest03StringExamplesStartsWith() throws Exception {
+        try {
+            assertEquals(exampleStartsWith(null, STARTS), STARTS);
+        } catch (AssertionError e) {
+            LOG.error(e.getStackTrace()[0]);
+            LOG.error(e.getStackTrace()[1]);
+        }
+        String methodName = Thread.currentThread()
+                .getStackTrace()[1]
+                .getMethodName()
+                .concat("()");
+        LOG.info(String.format("method <%s> passed successful", methodName));
+    }
+
+    @Test
+    public void negativeTest04StringExamplesStartsWith() throws Exception {
+        try {
+            assertEquals(exampleStartsWith(TEXT, null), STARTS);
+        } catch (AssertionError e) {
+            LOG.error(e.getStackTrace()[0]);
+            LOG.error(e.getStackTrace()[1]);
+        }
+        String methodName = Thread.currentThread()
+                .getStackTrace()[1]
+                .getMethodName()
+                .concat("()");
+        LOG.info(String.format("method <%s> passed successful", methodName));
+    }
+
+    @Test
+    public void negativeTest05StringExamplesStartsWith() throws Exception {
+        try {
+            assertEquals(exampleStartsWith(TEXT, STARTS), null);
         } catch (AssertionError e) {
             for (int i = 0; i < e.getStackTrace().length; i++) {
                 LOG.error(e.getStackTrace()[i]);
