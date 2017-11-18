a5e1303e29aab85b6b49032b0150fd2bd8f89ac1
alexace013 <alexace013@gmail.com>
2017-11-18 13:54:38 +0200

    modified SimpleCalcTests class

--- src/test/java/examples/simpleCalc/SimpleCalcTests.java
+++ src/test/java/examples/simpleCalc/SimpleCalcTests.java
@@ -1,10 +1,12 @@
 package examples.simpleCalc;
 
+import lombok.extern.java.Log;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
+@Log
 class SimpleCalcTests {
 
     private static CalcControl control;
@@ -22,41 +24,49 @@ class SimpleCalcTests {
 
     @Test
     void testSumPositive() {
+        log.info(SimpleCalcTests.class.getMethods()[0].getName());
         assertEquals(POSITIVE_MESSAGE, VALUE_5, control.sum(VALUE_2, VALUE_3));
     }
 
     @Test
     void testSumNegative() {
+        log.info(SimpleCalcTests.class.getMethods()[0].getName());
         assertNotEquals(NEGATIVE_MESSAGE, VALUE_2, control.sum(VALUE_2, VALUE_2));
     }
 
     @Test
     void testDiffPositive() {
+        log.info(SimpleCalcTests.class.getMethods()[0].getName());
         assertEquals(POSITIVE_MESSAGE, VALUE_2, control.difference(VALUE_5, VALUE_3));
     }
 
     @Test
     void testDiffNegative() {
+        log.info(SimpleCalcTests.class.getMethods()[0].getName());
         assertNotEquals(NEGATIVE_MESSAGE, VALUE_6, control.difference(VALUE_6, VALUE_5));
     }
 
     @Test
     void testMultPositive() {
+        log.info(SimpleCalcTests.class.getMethods()[0].getName());
         assertEquals(POSITIVE_MESSAGE, VALUE_6, control.multiplication(VALUE_2, VALUE_3));
     }
 
     @Test
     void testMultNegative() {
+        log.info(SimpleCalcTests.class.getMethods()[0].getName());
         assertNotEquals(NEGATIVE_MESSAGE, VALUE_2, control.multiplication(VALUE_6, VALUE_3));
     }
 
     @Test
     void testDivPositive() {
+        log.info(SimpleCalcTests.class.getMethods()[0].getName());
         assertEquals(POSITIVE_MESSAGE, VALUE_2, control.division(VALUE_6, VALUE_3));
     }
 
     @Test
     void testDivNegative() {
+        log.info(SimpleCalcTests.class.getMethods()[0].getName());
         assertNotEquals(NEGATIVE_MESSAGE, VALUE_6, control.division(VALUE_6, VALUE_2));
     }
 
