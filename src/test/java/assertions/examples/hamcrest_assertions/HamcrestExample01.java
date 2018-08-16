5587b7faed85c4901e1b5f038c172832f699d7aa
alexace013 <alexace013@gmail.com>
2018-08-16 19:55:02 +0300

    added hamcrest dependency into pom.xml file and simple Hamcrest example

---
+++ src/test/java/assertions/examples/hamcrest_assertions/HamcrestExample01.java
@@ -0,0 +1,25 @@
+package assertions.examples.hamcrest_assertions;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.Every.everyItem;
+
+public class HamcrestExample01 {
+
+    @Test
+    public void listShouldInitiallyBeEmpty() {
+        List<Integer> list = Arrays.asList(5, 2, 4);
+        assertThat(list, hasSize(3));
+        // ensure the order is correct
+        assertThat(list, contains(5, 2, 4));
+        assertThat(list, containsInAnyOrder(2, 4, 5));
+        assertThat(list, everyItem(greaterThan(1)));
+    }
+}
