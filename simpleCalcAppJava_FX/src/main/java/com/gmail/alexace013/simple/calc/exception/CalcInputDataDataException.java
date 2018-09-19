c10f91d849ccdaccd7ee3b0b5b9808d7f6d6b6f2
alexace013 <alexace013@gmail.com>
2018-09-19 23:57:19 +0300

    deleted simpleCalcAppJava_FX module

--- simpleCalcAppJava_FX/src/main/java/com/gmail/alexace013/simple/calc/exception/CalcInputDataDataException.java
+++
@@ -1,9 +0,0 @@
-package com.gmail.alexace013.simple.calc.exception;
-
-public class CalcInputDataDataException extends Exception {
-
-    public CalcInputDataDataException(final String message) {
-        super(message);
-    }
-
-}
--- simpleCalcAppJava_FX/src/main/java/com/gmail/alexace013/simple/calc/exception/PropertyReaderUtilException.java
+++
@@ -1,9 +0,0 @@
-package com.gmail.alexace013.simple.calc.exception;
-
-public class PropertyReaderUtilException extends Exception {
-
-    public PropertyReaderUtilException(final String message) {
-        super(message);
-    }
-
-}
